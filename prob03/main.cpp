10b3dc55719e545ce797a1a5ca168b160759b709
Leobardo.arevalo25 <Leobardo.arevalo25@gmail.com>
2019-08-31 16:26:19 -0700

    finish prob3 lab2

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,2 +1,25 @@
-// Name: 
+// Name:
+// This program calculates the area of a room.
+
+#include <iostream>
+
+int main()
+{
+  double tax,tip,total_bill,total_meal_cost;
+
+
+  // welcome to the restaurant bill calculator!.
+  std::cout << "What is the total meal cost?";
+  std::cin >> total_meal_cost;
+
+
+  // Calculate the tax and tip.
+  tip = 0.2 * total_meal_cost;
+  tax = .0775 * total_meal_cost;
+  total_bill= tip + tax + total_meal_cost;
+  std::cout << "The tax " <<tax<< "\ntip " <<tip<< "\ntotal_meal_cost " <<total_bill<<"\n";
+
+  return 0;
+}
+// Name:
 // This program calculates the tax and tip on a restaurant bill.
